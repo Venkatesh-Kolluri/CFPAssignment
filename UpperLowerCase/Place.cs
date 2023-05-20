6e6f64516f2071d1b594143af9eb889919c645ee
venky <venky94.vk@gmail.com>
2023-05-20 07:25:50 +0530

    [File added Successfully]

---
+++ UpperLowerCase/Place.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpperLowerCase
+{
+    internal class Place
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the place you want to visit:");
+            string input = Console.ReadLine();
+            string upperCase = input.ToUpper();
+            Console.WriteLine($"Place you want to visit in UpperCase ==> {upperCase}");
+            string lowerCase = input.ToLower();
+            Console.WriteLine($"Place you want to visit in LowerCase==> {lowerCase}");
+        }
+        
+    }
+}
