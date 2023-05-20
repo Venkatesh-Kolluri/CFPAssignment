f002604c873224046bbca7dd824bf963b6c99266
venky <venky94.vk@gmail.com>
2023-05-20 07:40:58 +0530

    [File factorial added successfully]

---
+++ Factorial/FactorialProgram.cs
@@ -0,0 +1,24 @@
+﻿namespace Factorial
+{
+    internal class FactorialProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to find factorial");
+            int num=int.Parse(Console.ReadLine());
+
+            int fact = Factorial(num);
+            Console.WriteLine("The factorial value is: "+fact);
+        }
+        public static int Factorial(int value)
+        {
+            int result = 1;
+            for(int i = 2; i <= value; i++)
+            {
+                result=result*i;
+
+            }
+            return result;
+        }
+    }
+}
