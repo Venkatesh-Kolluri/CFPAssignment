bf8cd5601edc6c7225aa54eeb376ab3163c9c226
venky <venky94.vk@gmail.com>
2023-05-20 07:37:29 +0530

    [File TIme added successfully]

---
+++ TIme/TimeSpan.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace Time
+{
+    internal class TimeSpan
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Console.WriteLine("Hello, World!");
+            System.Threading.Thread.Sleep(2000);
+            sw.Stop();
+            System.TimeSpan elapsed = sw.Elapsed;
+            Console.WriteLine("Execution Time: "+elapsed);
+        }
+    }
+}
