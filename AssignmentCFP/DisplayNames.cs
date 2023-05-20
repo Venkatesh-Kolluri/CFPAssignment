92d04afa1615e1b40f0f0ab699a1e56f8d6a417d
venky <venky94.vk@gmail.com>
2023-05-20 07:16:37 +0530

    [File added successfully]

---
+++ AssignmentCFP/DisplayNames.cs
@@ -0,0 +1,16 @@
+﻿namespace AssignmentCFP
+{
+    internal class DisplayNames
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Dinesh", "Prashant", "Rushi", "Sagar", "Ganesh" };
+            int i = 0;
+            while(i < names.Length)
+            {
+                Console.WriteLine((i+1)+") "+names[i]);
+                i++;
+            }
+        }
+    }
+}
