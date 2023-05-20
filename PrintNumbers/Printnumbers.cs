5ed690af0bb327b839ff196d79220e945aa6a845
venky <venky94.vk@gmail.com>
2023-05-20 07:48:59 +0530

    [File added successfully]

---
+++ PrintNumbers/Printnumbers.cs
@@ -0,0 +1,24 @@
+﻿namespace PrintNumbers
+{
+    internal class Printnumbers
+    {
+        public static void Repeat(int num)
+        {
+            if (num == 100)
+            {
+                Console.Write(num);
+            }
+            else
+            {
+                Console.Write(num+" ");
+                num++;
+                Repeat(num);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Printnumbers.Repeat(1);
+           
+        }
+    }
+}
