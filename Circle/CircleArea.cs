a1fca0dba3e7a3adc867ae07ad527a0834601e5b
venky <venky94.vk@gmail.com>
2023-05-20 07:34:45 +0530

    [File circle added successfully]

---
+++ Circle/CircleArea.cs
@@ -0,0 +1,16 @@
+﻿namespace Circle
+{
+    internal class CircleArea
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Diameter of Circle: ");
+            double d=Convert.ToDouble(Console.ReadLine());
+            double r = d / 2;
+            double perimeterOfCircle = 2 * (Math.PI) * r;
+            Console.WriteLine($"Perimeter of circle is: {perimeterOfCircle}");
+            double areaOfCircle = Math.PI * r * r;
+            Console.WriteLine($"Area of Circle is: {areaOfCircle}");
+        }
+    }
+}
