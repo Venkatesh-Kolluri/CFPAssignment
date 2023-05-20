5f50071505371c2f84148e829deb4a87cddb0d67
venky <venky94.vk@gmail.com>
2023-05-20 07:39:12 +0530

    [Divisible file added successfully]

---
+++ Divisible/Divisible.cs
@@ -0,0 +1,21 @@
+﻿namespace Divisible
+{
+    internal class Divisible
+    {
+        static void Main(string[] args) 
+        {
+            int start = 2000;
+            int end = 3200;
+
+            string result = "";
+            for(int i = start; i <= end; i++)
+            {
+                if(i % 7==0 && i%5 != 0)
+                {
+                   result += i+",";
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
