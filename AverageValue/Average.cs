765f45c7a640388db7c699e5418c40a1225cecae
venky <venky94.vk@gmail.com>
2023-05-20 07:33:05 +0530

    [File added Successfully]

---
+++ AverageValue/Average.cs
@@ -0,0 +1,19 @@
+﻿namespace AverageValue
+{
+    internal class Average
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int num1 = random.Next(10,50);
+            int num2=random.Next(10,50);
+            int num3=random.Next(10,50);
+            int num4=random.Next(10,50);
+            int num5=random.Next(10,50);
+            Console.Write( $"5 random values are: { num1} { num2} { num3} { num4} { num5}" );
+
+            int average = (num1 + num2 + num3 + num4 + num5) / 5;
+            Console.WriteLine( $"\nThe average value of 5 random numbres ==> {average}" );
+        }
+    }
+}
