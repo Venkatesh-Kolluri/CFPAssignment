425babe24ae57234e06ad90a7da819678d347b48
venky <venky94.vk@gmail.com>
2023-05-20 07:28:06 +0530

    [File added successfully]

---
+++ PrintString/NumString.cs
@@ -0,0 +1,16 @@
+﻿namespace PrintString
+{
+    internal class NumString
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a value:");
+            string num=Console.ReadLine();
+
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine((i+1)+") "+num);
+            }
+        }
+    }
+}
