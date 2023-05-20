b0cec7f178b977a9d46d92c0bf609ff879091430
venky <venky94.vk@gmail.com>
2023-05-20 07:36:14 +0530

    [File added Successfully]

---
+++ NextDayDate/Date.cs
@@ -0,0 +1,19 @@
+﻿namespace NextDayDate
+{
+    internal class Date
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a Year: ");
+            int year=int.Parse(Console.ReadLine());
+            Console.Write("\nInput a Month[1-12]: ");
+            int month=int.Parse(Console.ReadLine());
+            Console.Write("\nInput a Day[1-31]: ");
+            int day=int.Parse(Console.ReadLine());
+
+            DateTime currentDate=new DateTime(year, month, day);
+            DateTime nextDate=currentDate.AddDays(1);
+            Console.WriteLine("The next date is [yyyy-mm-dd] {0}",nextDate.ToString("yyyy-MM-dd"));
+        }
+    }
+}
