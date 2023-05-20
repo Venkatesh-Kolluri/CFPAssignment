632b2b22df2b360af5c16000e1a28cab0136f427
venky <venky94.vk@gmail.com>
2023-05-20 07:50:11 +0530

    [File added successfully]

---
+++ FindingElements/FindingElements.cs
@@ -0,0 +1,30 @@
+﻿namespace FindingElements
+{
+    internal class FindingElements
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a string: ");
+            string userInput = Console.ReadLine();
+
+            bool isAPresent = userInput.Contains("a");
+            bool isEPresent = userInput.Contains("e");
+            bool isPPresent = userInput.Contains("p");
+
+            if (isAPresent && isEPresent && isPPresent)
+            {
+                Console.WriteLine("All Present");
+            }
+            else if (isAPresent || isEPresent || isPPresent)
+            {
+                Console.WriteLine("One or more - Present");
+            }
+            else
+            {
+                Console.WriteLine("None Present");
+            }
+        }
+    }
+
+}
