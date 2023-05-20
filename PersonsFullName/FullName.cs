722a1ac3c848e1d1641a3c8ceaae69292bc692b9
venky <venky94.vk@gmail.com>
2023-05-20 07:29:37 +0530

    [File added successfully]

---
+++ PersonsFullName/FullName.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonsFullName
+{
+    internal class FullName
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter FirstName: ");
+            string firstName=Console.ReadLine();
+         
+            Console.WriteLine("Enter MiddleName: ");
+            string middleName=Console.ReadLine();
+         
+            Console.WriteLine("Enter LastName: ");
+            string lastName=Console.ReadLine();
+
+            string[] name = { firstName, middleName, lastName };
+
+            int Count= name.Length;
+            string fullName = string.Join(" ", name);
+
+            Console.WriteLine("Number of parts in the name: "+Count);
+            Console.WriteLine("FullName of a person is: "+fullName);
+            
+        }
+    }
+}
